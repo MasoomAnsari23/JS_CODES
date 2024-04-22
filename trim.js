22720cf2f208b952a22712167a3a1e04de69690a
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-22 15:08:10 +0530

    removing space by trim

---
+++ trim.js
@@ -0,0 +1,9 @@
+let firstName="   masoom  ";
+console.log(firstName);
+firstName=firstName.trim();
+console.log(firstName);
+
+let lastName="     ansari    ";
+console.log(lastName);
+let newlastName=lastName.trim();
+console.log(newlastName);
