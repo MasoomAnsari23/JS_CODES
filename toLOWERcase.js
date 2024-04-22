9f3bf8e39470073a1280c7ea4682fb096803d83f
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-22 15:06:44 +0530

    converting to lower case

---
+++ toLOWERcase.js
@@ -0,0 +1,4 @@
+let firstName="MASOOM";
+console.log(firstName);
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName);  /*here upper case will print because lower case will not change original value*/
