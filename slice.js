9b0a8184e7324aacf238ca4d430a2418f813dfa4
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-22 17:04:13 +0530

    slice

---
+++ slice.js
@@ -0,0 +1,7 @@
+let firstName="   Masoom";
+console.log(firstName);
+console.log(typeof firstName);
+console.log(firstName.length);
+
+console.log(firstName.slice(3,9)); 
+// so this slice will print only fronm string index 3 to 8
