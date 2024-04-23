9e506dbb8b2243f37d1662681fe7af5bb347b4c2
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-23 22:24:33 +0530

    string template

---
+++ templateSTRING.js
@@ -0,0 +1,5 @@
+let Name="Masoom Ansari";
+let age=20;
+console.log("my name is " + Name +" and my age is " +age);
+let aboutMe=`my name is ${Name} and my age is ${age}`; //this is string template
+console.log(aboutMe);
