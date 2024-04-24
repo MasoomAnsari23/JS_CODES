2e4d32e963082920c510d46b82c18beabf497e9f
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-24 10:58:56 +0530

    int vs bigint

---
+++ intBIGINT.js
@@ -0,0 +1,7 @@
+let a= 12423;
+console.log(typeof a);
+ let b=BigInt(12423);
+ let c=12423567n;
+ console.log(typeof b , b);
+ console.log(typeof c , c);
+ // we can't add bigint and int we have to conver them into same type
