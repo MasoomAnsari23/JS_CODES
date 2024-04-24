305a15795bb84d5fb5d98229153764d86d2943e1
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-24 22:12:24 +0530

    ternary operator file

---
+++ ternaryOPERATOR.js
@@ -0,0 +1,11 @@
+let age1=10;
+if(age1<10){
+    console.log("drinks milk");
+}             // this is if else
+else{
+    console.log("drinks coffee");
+}
+
+let age2=12;  //this is ternary operator
+let drink=age2<10 ? "milk": "coffee";
+console.log(drink);
