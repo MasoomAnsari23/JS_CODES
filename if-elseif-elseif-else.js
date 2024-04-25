cb9a99f8e26e243f80275bea6bac3e40290333be
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-25 22:16:11 +0530

    if else if else if else file

---
+++ if-elseif-elseif-else.js
@@ -0,0 +1,16 @@
+let temp=4;
+ if(temp<0){
+    console.log("extreame cold");
+ }
+ else if(temp>0 && temp<15){
+    console.log("cold");
+ }
+ else if(temp>15 && temp<40){
+    console.log("normal");
+ }
+ else if(temp>40 && temp<50){
+    console.log("hot");
+ }
+ else{
+    console.log("extreame temp");
+ }
