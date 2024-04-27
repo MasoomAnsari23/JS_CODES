f47c921b7b0a39ec3a0a9535c53e3bc1c20ac202
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-27 18:12:16 +0530

    shift and unshift

---
+++ array/shiftUNSHIFT.js
@@ -0,0 +1,10 @@
+let numb=[2,4,6,8,10];
+console.log(numb);
+
+let shiftelement;
+shiftelement=numb.shift();
+console.log(shiftelement); // element to be shifted
+console.log(numb);
+
+numb.unshift(0);    //element to be unshifted
+console.log(numb);
