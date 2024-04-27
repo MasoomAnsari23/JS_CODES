84899ff58aee5ff3ff9349cbc561815c7a836136
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-27 17:21:24 +0530

    updating values of array

---
+++ array/updatingARRAY.js
@@ -0,0 +1,14 @@
+let mixarr=["jamun" , 2 , "mango" , 4];
+console.log(mixarr);
+console.log(mixarr[0]);
+console.log(mixarr[1]);
+console.log(mixarr[2]);
+
+//updating values
+mixarr[0]="apple";
+mixarr[2]=23;
+mixarr[3]="four";
+console.log(mixarr);
+console.log(mixarr[0]);
+console.log(mixarr[2]);
+console.log(mixarr[3]);
