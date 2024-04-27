99149607bb87569020275b2fd57e614cc96ade7f
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-27 17:51:45 +0530

    push and pop in array

---
+++ array/arrayPUSHpop.js
@@ -0,0 +1,14 @@
+let numb=[2,3,5,7,9, "mango"];
+console.log(numb);
+numb.push("banana"); //this is push
+console.log(numb);
+console.log(numb[6]);
+
+//pushed element cam be stored creating new variabl and adding it into array
+
+//storing poped element
+let popedelement=numb.pop();
+console.log(popedelement)
+
+numb.pop();
+console.log(numb); //this is pop
