7f6ef888d63d8d1ed681ecc4e7cf6b922070fd09
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-28 19:14:16 +0530

    updating with all methods to add item in cloned array

--- array/addITEMinCLONEarray.js
+++ array/addITEMinCLONEarray.js
@@ -2,6 +2,16 @@ let arr1=[1,3,5,7];
 let arr2=arr1;
 console.log(arr1);
 console.log(arr2);
+
+// adding by slice method
 arr2=arr1.slice(0).concat([9]);
 console.log(arr1);
 console.log(arr2);
+
+//adding by concat method
+arr3=[].concat(arr1 , 13);
+console.log(arr3);
+
+//adding by spread method
+arr4=[...arr1, 23];
+console.log(arr4);
