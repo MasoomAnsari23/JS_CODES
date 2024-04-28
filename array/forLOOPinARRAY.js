98ec1239c86ba6ded028a1681f12abf4b85b7884
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-28 23:05:16 +0530

    for loop in array

---
+++ array/forLOOPinARRAY.js
@@ -0,0 +1,10 @@
+let arr1=[1,2,3,4,5,6,7,8,9,10];
+console.log(arr1);
+
+for(let i=0; i<=10; i++){
+console.log(i);  // this is just to print 0 to 10
+}
+
+for(let i=0; i<=arr1.length-1; i++){
+    console.log(arr1[i]);
+}
