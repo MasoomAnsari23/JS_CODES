7b78611fcde5e6914b5170d68115d6814669be8e
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-29 15:48:23 +0530

    adding key value to object

---
+++ objects/addingVALUE.js
@@ -0,0 +1,14 @@
+let obj1={
+    name:"Masoom",
+    age:20,
+    "person hobbies":["study", "play", "salah"], //this is array inside object
+}
+console.log(obj1);
+console.log(obj1["person hobbies"]);
+
+//ading key value to object
+
+let key="email";
+obj1[key]="masoom6ansari@gmail.com";
+console.log(obj1);
+
