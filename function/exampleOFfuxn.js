2fe02b8ae62af6d93648f91ad5c71c2d489bc5f2
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-30 11:44:12 +0530

    exapmle of function with all three methods

---
+++ function/exampleOFfuxn.js
@@ -0,0 +1,19 @@
+//  findin sum of two nos
+
+//meethod 1
+function sum1(a,b){
+console.log(a+b);
+}
+sum1(2,4);
+
+//method 2:function expression
+let sum2=function(a,b){
+    console.log(a+b);
+}
+sum2(5,6);
+
+//method 3: arrow method
+let sum3=(a,b)=>{
+    console.log(a+b);
+}
+sum3(7,8);
