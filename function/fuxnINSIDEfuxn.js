53cfdb83c02633a628dc619c856f9817fb5e0f48
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-30 15:19:27 +0530

    function inside function

---
+++ function/fuxnINSIDEfuxn.js
@@ -0,0 +1,16 @@
+let calC=()=>{
+    console.log("welcome");
+    {
+        const sum=(a,b)=>{
+        return a+b;
+            // console.log(a+b);
+        }
+        const mul=(a,b)=>{
+            return a*b;
+            // console.log(a*b);
+        }
+        console.log(sum(2,3));
+            console.log(mul(2,3));
+    }
+}
+calC();
