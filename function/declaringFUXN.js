bcd61eb131d8c2cfcea7d9848df73d46b35d4966
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-30 11:36:11 +0530

    all three methods of declaring function

---
+++ function/declaringFUXN.js
@@ -0,0 +1,20 @@
+//method 1
+function m1(){
+    console.log("happy birthday");
+}
+
+m1();  //calling function
+
+//method 2:function expression
+let m2= function(){
+    console.log("happy birthday");
+}
+
+m2();
+
+//method 3 : arrow function
+let m3=()=>{
+    console.log("happy birthday");
+}
+
+m3();
