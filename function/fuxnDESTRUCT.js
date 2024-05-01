95068c156e8b484baa218cdbb279575beee3b2cb
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-01 21:46:49 +0530

    function destructuring

---
+++ function/fuxnDESTRUCT.js
@@ -0,0 +1,11 @@
+const person={
+    name:"Masoom",
+    age:20,
+    gender:"male",
+}
+console.log(person);
+
+//this is destructuring with normal way
+console.log(person.name);  
+console.log(person.gender);
+console.log(person.age);
