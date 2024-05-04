26020eb02679480a4a8e010ef3665054e2ce770d
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-04 06:33:42 +0530

    example in array methods

---
+++ impo ARRAY methods/example1.js
@@ -0,0 +1,15 @@
+ //using for loop to print element of arrays
+ const arr1=[2,3,4,5,6];
+ let i=0;
+ for(i=0;i<arr1.length;i++){
+ console.log(arr1[i]);
+ }  
+
+
+ //using function to print member of arrays
+
+  const arr2=[3,5,7,9,1];
+  function arr2member(number,index){
+     console.log(number);
+  }
+   arr2member(arr2);
