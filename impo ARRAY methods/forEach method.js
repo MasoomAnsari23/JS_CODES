d3dcd1740554884a7740a8bc72347c2d08df2530
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-04 06:47:54 +0530

    method 2

--- impo ARRAY methods/forEach method.js
+++ impo ARRAY methods/forEach method.js
@@ -3,3 +3,8 @@ function myfunc(number,index){
     console.log(`index is ${index}: number is ${number}`);
 }
 arr1.forEach(myfunc);
+
+//simple method 
+arr1.forEach(function(number){
+    console.log(`number is ${number}`);
+});
