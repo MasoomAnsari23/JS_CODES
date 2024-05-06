e0a1625f311d8fc15f2ce0c18e564a44d3c7e91f
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-06 19:36:37 +0530

    iterable

---
+++ more useful things/iterable.js
@@ -0,0 +1,17 @@
+//string
+const st1="masoom";
+for (let str11 of st1){
+    console.log(str11);
+}
+
+//arrays
+const arr1=["item1", "item2", "item3"];
+for(let arr11 of arr1){
+    console.log(arr1);
+}
+
+//object will not work in for of loop
+// const obj1{"item1":"value1", "item2":"value2"};
+// for(obj11 of obj1){
+//     console.log(obj1);
+// }
