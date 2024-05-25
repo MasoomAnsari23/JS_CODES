d5a8fd25c68a815aac2e9d998942a5eb709dbbaf
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-25 20:19:23 +0530

    style

---
+++ DOM/creating website/style.js
@@ -0,0 +1,7 @@
+// const mainHeading=document.querySelector("div.headline h2"); both the lines are correct
+const mainHeading=document.querySelector("#main-heading");
+mainHeading.style.color="red";
+mainHeading.style.backgroundColor="white";
+mainHeading.style.border="10px,solid,green";
+
+//changes can be seen in website
