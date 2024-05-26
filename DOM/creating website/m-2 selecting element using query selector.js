1daa23ad816e985b9f605daf3e3e62b73a48bd73
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-26 21:46:37 +0530

    selecting element using query selector

---
+++ DOM/creating website/m-2 selecting element using query selector.js
@@ -0,0 +1,11 @@
+const mainHeading=document.querySelector("#main-header");
+console.log(mainHeading);
+
+const header=document.querySelector(".header");
+console.log(header);
+
+const navIteam=document.querySelector(".nav-item");
+console.log(navIteam);  //this will only read the first class value with nav-item
+
+const navitem=document.querySelectorAll(".nav-item");
+console.log(navitem);  //this will read every class with names as nav-item
