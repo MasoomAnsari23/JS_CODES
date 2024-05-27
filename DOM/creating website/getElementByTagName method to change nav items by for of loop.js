075b9facc5dd56780490396345f53a20b8cff241
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-27 21:46:21 +0530

    get element by tag name method

---
+++ DOM/creating website/getElementByTagName method to change nav items by for of loop.js
@@ -0,0 +1,6 @@
+const navIteams=document.getElementsByTagName("a");
+for(let navIteam of navIteams){
+    navIteam.style.color="green";
+    navIteam.style.backgroundColor="white";
+    navIteam.style.fontWeight="bold";
+}
