9151c93adc85124cf329007d8493e32292b76e56
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-28 22:02:49 +0530

    inner text

---
+++ DOM/creating website/innerText.js
@@ -0,0 +1,4 @@
+const mainHeading=document.getElementById("main-heading");
+console.log(mainHeading.innerText); // here hello is hidden
+
+console.log(mainHeading.textContent); // hello is not hidden
