68d6c3b886b1dc975705a96cc7e0746f7fcc5e58
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-29 08:53:46 +0530

    get multiple element by using query selector all

---
+++ DOM/creating website/get multiple element using querySelectorAll.js
@@ -0,0 +1,7 @@
+const navIteam=document.querySelectorAll("nav-item");
+console.log(navIteam);// NODELIST
+
+console.log(typeof navIteam);//object
+
+console.log(Array.isArray(navIteam));// not array
+console.log(navIteam[0]);//undefine
