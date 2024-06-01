4a173edfbed56fae00cec94c143437654049b501
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-06-01 22:20:48 +0530

    getElementByClassName

---
+++ DOM/creating website/get multiple element using getElementbyClassName.js
@@ -0,0 +1,8 @@
+const navItem=document.getElementsByClassName("nav-item");
+console.log(navItem); //HTML collection
+
+console.log(typeof navItem);
+console.log(Array.isArray(navItem));// not array
+console.log(navItem[0]);
+console.log(navItem[1]);
+console.log(navItem[2]);
