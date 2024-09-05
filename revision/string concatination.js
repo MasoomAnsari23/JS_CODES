31ff289116a25b8038ac24e55b5f3bf61e64c7e0
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-09-05 21:26:06 +0530

    string concatination

---
+++ revision/string concatination.js
@@ -0,0 +1,8 @@
+let str1="masoom";
+let str2="ansari";
+str3=str1+str2;
+console.log(str3);// it wont give space in between both string
+
+//to seperate both of them 
+str4=str1+" "+str2;
+console.log(str4);
