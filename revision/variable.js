7a2d7488735bb76402e76d8b48da193f892ca55f
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-09-05 20:49:20 +0530

    variable in revision

---
+++ revision/variable.js
@@ -0,0 +1,19 @@
+// using var to declare variable
+var firstName="gaza";
+console.log(firstName);
+firstName="palastine"; // we can use var here as well
+var firstName="hamas";
+console.log(firstName);
+// using let
+let lastName="ansari";
+console.log(lastName);
+
+lastName="khan";
+console.log(lastName);
+//we can't use let again here it will show error // let lastName="khan";
+
+//declaring constant
+const pi="3.142";
+console.log(pi);// we can not modify constant values// pi=3.45;
+// we can do operation with constant values
+console.log(pi*2);
