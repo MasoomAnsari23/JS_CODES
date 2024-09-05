10cabc275cb9fde6c8f00abab08439483df88898
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-09-05 21:19:18 +0530

    playing with data and datatype

---
+++ revision/playing with data and datatype.js
@@ -0,0 +1,16 @@
+let firstName="masoom";
+let age=22;
+console.log(firstName);
+//print datatype
+console.log(typeof (firstName));
+console.log(typeof age);
+
+// changing string to number
+firstName=+"masoom";
+console.log(typeof firstName);
+console.log(typeof(firstName));
+
+//converting number to string
+console.log(age);
+console.log(typeof age);
+console.log(typeof (age+""));
