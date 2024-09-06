81e5811558b872383c615c7bcac1751ffbba01e6
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-09-06 21:38:33 +0530

    template

---
+++ revision/template.js
@@ -0,0 +1,12 @@
+let firstName="masoom";
+let lastName="ansari";
+
+// using template to print firstNameand lastName
+let fullName=`my full name is ${firstName} ${lastName}`;
+console.log(fullName);
+
+//example 2
+let fullname="masoom";
+let age=23;
+let detail=`my name is ${fullname} and my age is ${age}`;
+console.log(detail);
