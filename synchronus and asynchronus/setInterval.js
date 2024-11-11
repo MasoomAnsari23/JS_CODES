5dd66741efb1f74650d120e1ff27b51587f7e1da
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-11-11 19:07:16 +0530

    setInterval

---
+++ synchronus and asynchronus/setInterval.js
@@ -0,0 +1,8 @@
+console.log("script start");
+
+setInterval(()=>{
+    console.log("free palastine");
+    console.log(Math.random());//it will print random number
+},500);
+
+console.log("script ends");
